ebb6a36f084cf6b3794087a0380ff92449c0f5b8
Patrick Weingaertner <patrick.weingaertner91@gmail.com>
2018-01-09 09:51:51 +0100

    add temporary icon. add delete button

--- listSessions.js
+++ listSessions.js
@@ -1,18 +1,16 @@
-async function openSessionList() {
-  //asynchron muss mit callback oder promise arbeiten
+async function loadSessionList() {
   let sessions = await getSavedSessions()
-  //somehow print the sessions as clickable itmes
-  $("#SessionList").append("<ul></ul>")
-  for(var i in sessions) {
-    var li = "<li>";
-    $("ul").append(li.concat(sessions[i]))
-  }
+
+  $.each(sessions, (key, value) => {
+    $("#SessionList").append(`<option value=${key}>${key}</option><`)
+  })
 }
 
 
 function getSavedSessions() {
   return new Promise((resolve, reject) => {
     chrome.storage.sync.get((sessions) => {
+      
       if(!chrome.runtime.lastError) {
         resolve(sessions)
       } else {
@@ -22,14 +20,38 @@ function getSavedSessions() {
   })
 }
 
-function removeSavedSession(sessionName) {
+function restoreSavedSession() {
+  var session = document.getElementById("SessionList");
+  var sessionKey = session.options[session.selectedIndex].value;
+  
+  chrome.storage.sync.get(sessionKey, (sessionObj) => {
+    chrome.tabs.query({}, function (tabs) {
+      
+      for (var i = 0; i < tabs.length; i++) {
+        chrome.tabs.remove(tabs[i].id);
+      }
+    })
 
+    let links = sessionObj[sessionKey]
+    
+    for(let link of links) {      
+      chrome.tabs.create({url:link})
+    }
+  })
+}
+
+function removeSavedSession() {
+  var session = document.getElementById("SessionList");
+  var sessionKey = session.options[session.selectedIndex].value;
+  chrome.storage.sync.remove(sessionKey)
+
+  //reload page and new list
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  let test = document.getElementById('test')
-
-  test.addEventListener('click', () => {
-    openSessionList()
-  })
+    loadSessionList()
+    let restore = document.getElementById('restore')
+    let remove = document.getElementById('remove')
+    restore.addEventListener('click', () => restoreSavedSession())
+    remove.addEventListener('click', () => removeSavedSession())
 })
--- popup.js
+++ popup.js
@@ -1,6 +1,4 @@
 function getAllUrls(sessionName) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   let queryInfo = {
     currentWindow: true
   };
@@ -20,27 +18,9 @@ function saveSession(sessionName, tabs) {
   chrome.storage.sync.set(session)
 }
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  let script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   let saveButton = document.getElementById('savebtn')
   
-
   saveButton.addEventListener('click', () => {
     let sessionName = document.getElementById('sessionname').value
     if(sessionName) {
